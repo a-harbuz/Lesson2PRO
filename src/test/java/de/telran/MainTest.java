3e0e8fa3419e20a3f838132070bedf74596b4161
pamail08@gmail.com <pamail08@gmail.com>
2024-02-12 00:03:20 +0100

    update1

--- src/test/java/de/telran/MainTest.java
+++ src/test/java/de/telran/MainTest.java
@@ -1,4 +1,25 @@
+package de.telran;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+//    @Spy
+//    Main main;
+    Main main = Mockito.mock(Main.class);
+    @Test
+    void mainTest() {
+        Mockito.when(main.makePaymentTest()).thenReturn(true);
+        boolean actualResult = main.makePaymentTest();
+        Assertions.assertTrue(actualResult);
+        //Mockito.verify(System.out,Mockito.times(5)).println(Mockito.anyString());
+    }
 }
